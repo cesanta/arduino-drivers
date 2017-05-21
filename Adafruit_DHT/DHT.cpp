b381fc20576f9ee9a0d32f0798897d03897292d1
ruslanvaliullin <r.valiullin77@gmail.com>
2017-05-21 15:19:04 +0300

    Change api_arduino_dht.js
    PUBLISHED_FROM=8dda14bd80a023b17513178c47f040eee8167f56

--- Adafruit_DHT/DHT.cpp
+++ Adafruit_DHT/DHT.cpp
@@ -1,4 +1,5 @@
 /* DHT library
+
 MIT license
 written by Adafruit Industries
 */
@@ -8,13 +9,17 @@ written by Adafruit Industries
 #define MIN_INTERVAL 2000
 
 DHT::DHT(uint8_t pin, uint8_t type, uint8_t count) {
-  (void)count;
   _pin = pin;
   _type = type;
+  #ifdef __AVR
+    _bit = digitalPinToBitMask(pin);
+    _port = digitalPinToPort(pin);
+  #endif
   _maxcycles = microsecondsToClockCycles(1000);  // 1 millisecond timeout for
                                                  // reading pulses from DHT sensor.
   // Note that count is now ignored as the DHT reading algorithm adjusts itself
   // basd on the speed of the processor.
+  (void) count;
 }
 
 void DHT::begin(void) {
@@ -24,6 +29,7 @@ void DHT::begin(void) {
   // >= MIN_INTERVAL right away. Note that this assignment wraps around,
   // but so will the subtraction.
   _lastreadtime = -MIN_INTERVAL;
+  DEBUG_PRINT("Max clock cycles: "); DEBUG_PRINTLN(_maxcycles, DEC);
 }
 
 //boolean S == Scale.  True == Fahrenheit; False == Celcius
@@ -65,7 +71,6 @@ float DHT::convertFtoC(float f) {
 }
 
 float DHT::readHumidity(bool force) {
-  (void)force;
   float f = NAN;
   if (read()) {
     switch (_type) {
@@ -81,6 +86,7 @@ float DHT::readHumidity(bool force) {
       break;
     }
   }
+  (void) force;
   return f;
 }
 
@@ -142,38 +148,45 @@ boolean DHT::read(bool force) {
   delay(20);
 
   uint32_t cycles[80];
+  {
+    // Turn off interrupts temporarily because the next sections are timing critical
+    // and we don't want any interruptions.
+    InterruptLock lock;
 
-  // End the start signal by setting data line high for 40 microseconds.
-  digitalWrite(_pin, HIGH);
-  delayMicroseconds(40);
+    // End the start signal by setting data line high for 40 microseconds.
+    digitalWrite(_pin, HIGH);
+    delayMicroseconds(40);
 
-  // Now start reading the data line to get the value from the DHT sensor.
-  pinMode(_pin, INPUT_PULLUP);
-  delayMicroseconds(10);  // Delay a bit to let sensor pull data line low.
+    // Now start reading the data line to get the value from the DHT sensor.
+    pinMode(_pin, INPUT_PULLUP);
+    delayMicroseconds(10);  // Delay a bit to let sensor pull data line low.
 
-  // First expect a low signal for ~80 microseconds followed by a high signal
-  // for ~80 microseconds again.
-  if (expectPulse(LOW) == 0) {
-    _lastresult = false;
-    return _lastresult;
-  }
-  if (expectPulse(HIGH) == 0) {
-    _lastresult = false;
-    return _lastresult;
-  }
+    // First expect a low signal for ~80 microseconds followed by a high signal
+    // for ~80 microseconds again.
+    if (expectPulse(LOW) == 0) {
+      DEBUG_PRINTLN(F("Timeout waiting for start signal low pulse."));
+      _lastresult = false;
+      return _lastresult;
+    }
+    if (expectPulse(HIGH) == 0) {
+      DEBUG_PRINTLN(F("Timeout waiting for start signal high pulse."));
+      _lastresult = false;
+      return _lastresult;
+    }
 
-  // Now read the 40 bits sent by the sensor.  Each bit is sent as a 50
-  // microsecond low pulse followed by a variable length high pulse.  If the
-  // high pulse is ~28 microseconds then it's a 0 and if it's ~70 microseconds
-  // then it's a 1.  We measure the cycle count of the initial 50us low pulse
-  // and use that to compare to the cycle count of the high pulse to determine
-  // if the bit is a 0 (high state cycle count < low state cycle count), or a
-  // 1 (high state cycle count > low state cycle count). Note that for speed all
-  // the pulses are read into a array and then examined in a later step.
-  for (int i=0; i<80; i+=2) {
-    cycles[i]   = expectPulse(LOW);
-    cycles[i+1] = expectPulse(HIGH);
-  }
+    // Now read the 40 bits sent by the sensor.  Each bit is sent as a 50
+    // microsecond low pulse followed by a variable length high pulse.  If the
+    // high pulse is ~28 microseconds then it's a 0 and if it's ~70 microseconds
+    // then it's a 1.  We measure the cycle count of the initial 50us low pulse
+    // and use that to compare to the cycle count of the high pulse to determine
+    // if the bit is a 0 (high state cycle count < low state cycle count), or a
+    // 1 (high state cycle count > low state cycle count). Note that for speed all
+    // the pulses are read into a array and then examined in a later step.
+    for (int i=0; i<80; i+=2) {
+      cycles[i]   = expectPulse(LOW);
+      cycles[i+1] = expectPulse(HIGH);
+    }
+  } // Timing critical code is now complete.
 
   // Inspect pulses and determine which ones are 0 (high state cycle count < low
   // state cycle count), or 1 (high state cycle count > low state cycle count).
@@ -181,6 +194,7 @@ boolean DHT::read(bool force) {
     uint32_t lowCycles  = cycles[2*i];
     uint32_t highCycles = cycles[2*i+1];
     if ((lowCycles == 0) || (highCycles == 0)) {
+      DEBUG_PRINTLN(F("Timeout waiting for pulse."));
       _lastresult = false;
       return _lastresult;
     }
@@ -195,12 +209,21 @@ boolean DHT::read(bool force) {
     // stored data.
   }
 
+  DEBUG_PRINTLN(F("Received:"));
+  DEBUG_PRINT(data[0], HEX); DEBUG_PRINT(F(", "));
+  DEBUG_PRINT(data[1], HEX); DEBUG_PRINT(F(", "));
+  DEBUG_PRINT(data[2], HEX); DEBUG_PRINT(F(", "));
+  DEBUG_PRINT(data[3], HEX); DEBUG_PRINT(F(", "));
+  DEBUG_PRINT(data[4], HEX); DEBUG_PRINT(F(" =? "));
+  DEBUG_PRINTLN((data[0] + data[1] + data[2] + data[3]) & 0xFF, HEX);
+
   // Check we read 40 bits and that the checksum matches.
   if (data[4] == ((data[0] + data[1] + data[2] + data[3]) & 0xFF)) {
     _lastresult = true;
     return _lastresult;
   }
   else {
+    DEBUG_PRINTLN(F("Checksum failure!"));
     _lastresult = false;
     return _lastresult;
   }
@@ -215,10 +238,24 @@ boolean DHT::read(bool force) {
 //   https://github.com/arduino/Arduino/blob/master/hardware/arduino/avr/cores/arduino/wiring_pulse.c
 uint32_t DHT::expectPulse(bool level) {
   uint32_t count = 0;
-  while (digitalRead(_pin) == level) {
-    if (count++ >= _maxcycles) {
-      return 0; // Exceeded timeout, fail.
+  // On AVR platforms use direct GPIO port access as it's much faster and better
+  // for catching pulses that are 10's of microseconds in length:
+  #ifdef __AVR
+    uint8_t portState = level ? _bit : 0;
+    while ((*portInputRegister(_port) & _bit) == portState) {
+      if (count++ >= _maxcycles) {
+        return 0; // Exceeded timeout, fail.
+      }
     }
-  }
+  // Otherwise fall back to using digitalRead (this seems to be necessary on ESP8266
+  // right now, perhaps bugs in direct port access functions?).
+  #else
+    while (digitalRead(_pin) == level) {
+      if (count++ >= _maxcycles) {
+        return 0; // Exceeded timeout, fail.
+      }
+    }
+  #endif
+
   return count;
 }
